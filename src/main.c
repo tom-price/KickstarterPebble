362b90116dacfedf40312f0dc3d208f978c79ae9
Thomas Price <tom@tomprice.ca>
2015-03-02 12:09:57 -0500

    Changed fetch interval to 5 min

--- src/main.c
+++ src/main.c
@@ -107,7 +107,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 	update_time();
   
 	// Get Kickstarter Info update every 5 minutes
-	if(tick_time->tm_min % 1 == 0) {
+	if(tick_time->tm_min % 5 == 0) {
 		// Begin dictionary
 		DictionaryIterator *iter;
 		app_message_outbox_begin(&iter);
